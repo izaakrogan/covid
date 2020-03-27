a4151fbf262b37492c106306faf59472f73fcd6d
izaakrogan <izaakrogan@googlemail.com>
2020-03-27 19:59:59 +0000

    housekeeping

--- playground/BarChart.tsx
+++ playground/BarChart.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import * as d3 from 'd3';
-import colors from '../style/colors';
+import colors from '../src/style/colors';
 interface Props {
   id: string;
 }
